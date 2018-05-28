484d7344727666605609133942252f6301db2d89
Maria Dees <kingsgrl2010@yahoo.com>
2018-05-28 19:16:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+const main = document.getElementById('main')
+ 
+main.addEventListener('click', function(event) {
+  alert('I was clicked!')
+})
+
+const input = document.querySelector('input')
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
+
